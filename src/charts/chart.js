b93cc98607b2cee369bb65f412dc81909b18234e
Chris Henrick <chrishenrick@gmail.com>
2018-03-12 17:27:45 -0700

    Revert "remove extra data clone in setData"
    This reverts commit 1243246ea96e46d6116c7891ffc4f595e0e5fb0d.

--- src/charts/chart.js
+++ src/charts/chart.js
@@ -3,6 +3,7 @@ import * as d3 from "./helpers/d3-service"
 import {colors} from "./helpers/colors"
 import {keys} from "./helpers/constants"
 import {
+  cloneData,
   override,
   throttle,
   rebind,
@@ -310,6 +311,7 @@ export default function Chart (_container) {
   }
 
   function setData (_data) {
+    dataObject.data = cloneData(_data[keys.SERIES])
     const cleanedData = dataManager.cleanData(_data, config.keyType)
     Object.assign(dataObject, cleanedData)
 

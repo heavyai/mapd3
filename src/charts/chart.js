b6d22563304a568154d20d585c12f894e20aa413
Chris Henrick <chrishenrick@gmail.com>
2018-03-15 17:30:01 -0700

    [WIP] svg chart class name (#19)
    add class name of chart type to svg el

--- src/charts/chart.js
+++ src/charts/chart.js
@@ -162,9 +162,28 @@ export default function Chart (_container) {
 
   function build () {
     if (!cache.svg) {
-      const template = `<div class="mapd3 mapd3-container">
+      const chartClassName = chartType => {
+          switch(chartType) {
+            case "bar":
+            case "stackedBar":
+              return "bar"
+
+            case "line":
+            case "stackedArea":
+              return "line"
+
+            // TO DO: handle bar line combo chartType...
+            case Array.isArray(chartType):
+              return "combo"
+
+            default:
+              return ""
+          }
+      }
+
+      const template = (chartType) => `<div class="mapd3 mapd3-container">
         <div class="header-group"></div>
-        <svg class="chart">
+        <svg class="chart ${chartClassName(chartType)}">
           <g class="chart-group"></g>
           <g class="panel-group">
             <rect class="panel-background"></rect>
@@ -174,7 +193,7 @@ export default function Chart (_container) {
       </div>`
 
       const base = d3.select(cache.container)
-          .html(template)
+          .html(template(config.chartType))
 
       cache.container = base.select(".mapd3-container")
           .style("position", "relative")
@@ -380,6 +399,7 @@ export default function Chart (_container) {
   }
 
   function render () {
+    console.log(config)
     build()
 
     if (dataObject.dataBySeries) {

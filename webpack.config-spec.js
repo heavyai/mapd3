1609a3a1d4004d91cd528400bbe3852e994c6f4f
Chris Viau <chrisv@mapd.com>
2018-04-02 11:53:01 -0400

    clean-up webpack config

--- webpack.config-spec.js
+++ webpack.config-spec.js
@@ -1,18 +1,23 @@
-var nodeExternals = require('webpack-node-externals');
+const path = require("path")
+const nodeExternals = require("webpack-node-externals")
+const bundleIndexPath = path.resolve("./src/bundle.js")
 
 module.exports = {
+  entry: {
+    mapd3: bundleIndexPath
+  },
   output: {
     // use absolute paths in sourcemaps (important for debugging via IDE)
-    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    devtoolFallbackModuleFilenameTemplate: "[absolute-resource-path]?[hash]"
   },
-  target: 'node',  // webpack should compile node compatible code
+  target: "node", // webpack should compile node compatible code
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   devtool: "inline-cheap-module-source-map",
   module: {
     rules: [
-      { test: /\.scss$/, loader: 'null-loader' },
-      { test: /\.css$/, loader: 'null-loader' },
+      {test: /\.scss$/, loader: "null-loader"},
+      {test: /\.css$/, loader: "null-loader"}
     ]
   }
-};
+}

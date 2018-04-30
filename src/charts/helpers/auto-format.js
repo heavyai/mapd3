1133ee61a276d29611ab39716ef6c515346ab3fc
Chris Viau <christopheviau@gmail.com>
2018-04-30 14:17:14 -0400

    Formatting module and specs (#41)
    * start of formatting module and specs
    * complete autoFormatter
    * key formats

---
+++ src/charts/helpers/auto-format.js
@@ -0,0 +1,127 @@
+import * as d3 from "./d3-service"
+
+/* eslint-disable no-magic-numbers */
+const prefixTranslation = {
+  k: Math.pow(10, 3),
+  K: Math.pow(10, 3),
+  m: Math.pow(10, 6),
+  M: Math.pow(10, 6),
+  g: Math.pow(10, 9),
+  G: Math.pow(10, 9),
+  t: Math.pow(10, 12),
+  T: Math.pow(10, 12)
+}
+
+function applyNumberFormat(format, value) {
+  try {
+    return d3.format(format)(value)
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn("Invalid format", format)
+    return value
+  }
+}
+
+function applyNumberFormatWithSuffix(tokens, value) {
+  if (
+    tokens.length === 2 &&
+    prefixTranslation[tokens[1]]
+  ) {
+    try {
+      return d3.formatPrefix(tokens[0], prefixTranslation[tokens[1]])(value)
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.warn("Invalid format", tokens)
+      return value
+    }
+  } else {
+    return value
+  }
+}
+
+function formatPrefixSuffix(format, value) {
+  // format like `foo {.2f} bar`
+  const tokens = format.split(/[{}]/)
+  const d3FormatToken = tokens[1]
+  const d3FormattedValue = applyNumberFormat(d3FormatToken, value)
+  return `${tokens[0]}${d3FormattedValue}${tokens[2]}`
+}
+
+function formatForcedSuffix(format, value) {
+  // format like `.2f|k`
+  const tokens = format.split("|")
+  return applyNumberFormatWithSuffix(tokens, value)
+}
+
+export function formatNumber(format, value) {
+  if (/[{}]/.test(format)) {
+    return formatPrefixSuffix(format, value)
+  } else if (/\|/.test(format)) {
+    return formatForcedSuffix(format, value)
+  } else {
+    return applyNumberFormat(format, value)
+  }
+}
+
+function validateDateFormat(format, value) {
+  const formatted = d3.timeFormat(format)(value)
+
+  return formatted !== format
+}
+
+function applyDateFormat(format, value) {
+  if (!validateDateFormat(format, value)) {
+    return String(value)
+  }
+  return d3.timeFormat(format)(value)
+}
+
+function formatDate(format, value) {
+  return applyDateFormat(format, value)
+}
+
+function parseType(format, value) {
+  if (typeof value === "number") {
+    return formatNumber(format, value)
+  } else if (value instanceof Date) {
+    return formatDate(format, value)
+  } else {
+    return String(value)
+  }
+}
+
+export default function autoFormatter(_format) {
+  let format = _format
+  // no format
+  if (!_format || (Array.isArray(_format) && _format.length === 0)) {
+    return null
+  }
+
+  return (value, key) => {
+
+    const hasKey = typeof key !== "undefined"
+
+    // pick format from key
+    if (Array.isArray(_format)) {
+      if (hasKey) {
+        const match = _format.filter(d => d.key === key)[0]
+        if (match && match.format) {
+          format = match.format
+        } else {
+          // no matching format
+          return null
+        }
+      } else {
+        format = _format[0] && _format[0].format
+      }
+    }
+
+    if (Array.isArray(value)) {
+      // format each element of an array of values
+      return value.map(d => parseType(format, d)).join(" - ")
+    } else {
+      // format value
+      return parseType(format, value)
+    }
+  }
+}
---
+++ test/formatter.spec.js
@@ -0,0 +1,164 @@
+import {expect} from "chai"
+import autoFormatter from "../src/charts/helpers/auto-format"
+/* eslint-disable no-unused-expressions, no-undefined */
+
+describe("Automatic Formatter", () => {
+
+  const BASE_NUMBER = 12345.678
+  const BASE_DATE = new Date("3/21/2018")
+
+  describe("Number formatters", () => {
+
+    it("should round to a number of decimals", () => {
+      const formatted = autoFormatter(".2f")(BASE_NUMBER)
+      expect(formatted.split(".")[1]).to.equal("68")
+    })
+
+    it("should add thousand separators", () => {
+      const formatted = autoFormatter(",")(BASE_NUMBER)
+      expect(formatted).to.equal("12,345.678")
+    })
+
+    it("should round to integer", () => {
+      const formatted = autoFormatter(".0f")(BASE_NUMBER)
+      expect(formatted).to.equal("12346")
+    })
+
+    it("should round to SI format with given precision", () => {
+      const formatted = autoFormatter(".2s")(BASE_NUMBER)
+      expect(formatted).to.equal("12k")
+    })
+
+    it("should convert to percentage, multiplying by 100", () => {
+      const formatted = autoFormatter(".0%")(0.25)
+      expect(formatted).to.equal("25%")
+    })
+
+    it("should convert to percentage with given precision", () => {
+      const formatted = autoFormatter(".2p")(BASE_NUMBER)
+      expect(formatted).to.equal("1200000%")
+    })
+
+    it("should convert to currency (locale specific)", () => {
+      const formatted = autoFormatter("-$.2f")(BASE_NUMBER)
+      expect(formatted).to.equal("$12345.68")
+    })
+
+    it("should use minus sign for negative currency", () => {
+      const formatted = autoFormatter("-$.2f")(-BASE_NUMBER)
+      expect(formatted).to.equal("-$12345.68")
+    })
+
+    it("should use parenthesis for negative currency", () => {
+      const formatted = autoFormatter("($.2f")(-BASE_NUMBER)
+      expect(formatted).to.equal("($12345.68)")
+    })
+
+    it("should force an SI suffix with given precision", () => {
+      const formatted = autoFormatter(",.3s|M")(BASE_NUMBER)
+      expect(formatted).to.equal("0.012M")
+    })
+
+    it("should add prefix/suffix", () => {
+      const formatted = autoFormatter("AVG {.2f}km/h")(BASE_NUMBER)
+      expect(formatted).to.equal("AVG 12345.68km/h")
+    })
+
+    it("should return the value on invalid formatter", () => {
+      const formatted = autoFormatter("aaaaa")(BASE_NUMBER)
+      expect(formatted).to.equal(BASE_NUMBER)
+    })
+
+    it("should return null if there's no formatter", () => {
+      const formatted = autoFormatter(null)
+      expect(formatted).to.be.null
+    })
+
+  })
+
+  describe("Date formatters", () => {
+
+    it("should handle typical date formats", () => {
+      const formatted = autoFormatter("%Y-%m-%d")(BASE_DATE)
+      expect(formatted).to.equal("2018-03-21")
+
+      const formatted2 = autoFormatter("%m/%d/%Y")(BASE_DATE)
+      expect(formatted2).to.equal("03/21/2018")
+
+      const formatted3 = autoFormatter("%c")(BASE_DATE)
+      expect(formatted3).to.equal("3/21/2018, 12:00:00 AM")
+    })
+
+    it("should handle invalid date formats", () => {
+      const formatted = autoFormatter("foo")(BASE_DATE)
+      expect(formatted).to.equal("Wed Mar 21 2018 00:00:00 GMT-0400 (EDT)")
+    })
+
+  })
+
+  describe("Value input", () => {
+
+    it("should handle array input", () => {
+      const formatted = autoFormatter(".2s")([1234.567, 89])
+      expect(formatted).to.equal("1.2k - 89")
+
+      const formatted2 = autoFormatter("%Y")([BASE_DATE])
+      expect(formatted2).to.equal("2018")
+    })
+
+    it("should pass through string input", () => {
+      const formatted = autoFormatter(".2s")("foo")
+      expect(formatted).to.equal("foo")
+    })
+
+    it("should convert unsupported value types to string", () => {
+      const formatted = autoFormatter(".2s")(undefined)
+      expect(formatted).to.equal("undefined")
+
+      const formatted2 = autoFormatter(".2s")({})
+      expect(formatted2).to.equal("[object Object]")
+
+      const formatted3 = autoFormatter(".2s")(null)
+      expect(formatted3).to.equal("null")
+    })
+
+  })
+
+  describe("Format by key", () => {
+
+    const formats = [
+      {key: "foo", format: ".2s"},
+      {key: "bar", format: ".2f"}
+    ]
+
+    it("should pick the format by key", () => {
+      const formatted = autoFormatter(formats)(BASE_NUMBER, "foo")
+      expect(formatted).to.equal("12k")
+
+      const formatted2 = autoFormatter(formats)(BASE_NUMBER, "bar")
+      expect(formatted2).to.equal("12345.68")
+    })
+
+    it("should return null if no match", () => {
+      const formatted = autoFormatter(formats)(BASE_NUMBER, "baz")
+      expect(formatted).to.be.null
+    })
+
+    it("should accept a null value, e.g. just to check if format exists for this key", () => {
+      const formatted = autoFormatter(formats)(null, "baz")
+      expect(formatted).to.be.null
+    })
+
+    it("should use the first format if key is not specified", () => {
+      const formatted = autoFormatter(formats)(BASE_NUMBER)
+      expect(formatted).to.equal("12k")
+    })
+
+    it("should return null if format array is empty", () => {
+      const formatted = autoFormatter([])
+      expect(formatted).to.be.null
+    })
+
+  })
+
+})
